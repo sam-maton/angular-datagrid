9da7468b0a63154847896a511137d19d1dfe3a76
Sam Maton <sam.maton91@gmail.com>
2024-09-02 12:29:43 +0100

    added datagrid component files

---
+++ src/app/datagrid/datagrid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datagrid',
+  standalone: true,
+  imports: [],
+  templateUrl: './datagrid.component.html',
+  styleUrl: './datagrid.component.css'
+})
+export class DatagridComponent {
+
+}
